78f7d2fe791a05f53691eb84590ff9005c07a282
JosueGalRe <josuegalre@gmail.com>
2022-10-31 18:25:25 -0600

    ✨ Added new util

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,9 +1,6 @@
-import type { ReactNode } from 'react';
-
-import { Unstable_Grid2 as Grid, Avatar } from '@mui/material';
 import { invoke } from '@tauri-apps/api';
 
-import { ChampionInfoResponse, State, Trees, Shards, RuneData, Shard } from 'interfaces';
+import { ChampionInfoResponse, State, RuneTrees, Shards, AutoCompleteOptions, ChampionInfo } from 'interfaces';
 import ValidatedStateResponse from 'interfaces/ValidatedStateResponse';
 
 async function getChampionInfo(state: State): Promise<ChampionInfoResponse> {
@@ -15,23 +12,21 @@ async function getChampionInfo(state: State): Promise<ChampionInfoResponse> {
       region: state.region,
     };
 
-    const [runes, shards, winRate] = await Promise.all([
-      invoke<Trees>('rune_names', requestArgs),
+    const [runes, shards, championInfo] = await Promise.all([
+      invoke<RuneTrees>('rune_names', requestArgs),
       invoke<Shards>('shard_names', requestArgs),
-      invoke<string>('win_rate', requestArgs),
+      invoke<ChampionInfo>('champion_info', requestArgs),
     ]);
 
-    return { runes, shards, winRate, completedSuccessfully: true };
+    return { ...championInfo, runes, shards, completedSuccessfully: true };
   } catch (exception) {
     console.error('Got an error while trying to fetch the runes for state %o: %o', state, exception);
     return { message: 'No Data Exists!', completedSuccessfully: false };
   }
 }
 
-async function getChampionNames() {
-  const championNames: string[] = await invoke('champion_names');
-
-  return championNames.map((name) => name.replace(/['"]+/g, ''));
+function getChampionNames() {
+  return invoke<AutoCompleteOptions[]>('champion_names');
 }
 
 const validateState = (state: State): ValidatedStateResponse => {
@@ -52,10 +47,6 @@ const validateState = (state: State): ValidatedStateResponse => {
   return { isValid: true, message: '' };
 };
 
-const runeMap = ({ name, image, active }: RuneData | Shard): ReactNode => (
-  <Grid key={name} sm sx={{ display: 'flex', alignSelf: 'center', justifyContent: 'center' }}>
-    <Avatar src={image} alt={name} sx={{ ...(!active && { filter: 'grayscale(100%)', opacity: '.3' }) }} />
-  </Grid>
-);
+const createArrayFromLength = (length: number) => [...Array(length)].map((_) => Math.ceil(Math.random() * 40 * length));
 
-export { getChampionInfo, getChampionNames, validateState, runeMap };
+export { getChampionInfo, getChampionNames, validateState, createArrayFromLength };

7e4124c53c3b9c4c4be1d6ca6a442655213b7c07
JosueGalRe <josuegalre@gmail.com>
2022-11-03 15:19:28 -0600

    ♻️ Refactored thread lock loop with promise.all

--- scripts/assets.js
+++ scripts/assets.js
@@ -1,30 +1,75 @@
+/* eslint-disable no-console */
 import fs from 'fs';
-import { WritableStream } from 'node:stream/web';
+import fsa from 'fs/promises';
+import path from 'path';
 
-let version = await fetch('https://ddragon.leagueoflegends.com/api/versions.json');
-version = await version.json();
-let runeJson = await fetch(`http://ddragon.leagueoflegends.com/cdn/${version[0]}/data/en_US/runesReforged.json`);
-runeJson = await runeJson.json();
+const rootFolder = path.resolve('./');
+
+const DDRAGON_URL = 'http://ddragon.leagueoflegends.com';
+
+const version = await (await fetch(`${DDRAGON_URL}/api/versions.json`)).json();
+const runePaths = await (await fetch(`${DDRAGON_URL}/cdn/${version[0]}/data/en_US/runesReforged.json`)).json();
+
+async function pathExists(filePath) {
+  try {
+    await fsa.access(filePath);
+    return true;
+  } catch {
+    return false;
+  }
+}
 
 const stream = (downloadStream) =>
   new WritableStream({
     write(chunk) {
       downloadStream.write(chunk);
     },
+    abort(reason) {
+      console.error(reason);
+    },
   });
 
-for (let y = 0; y < runeJson.length; y += 1) {
-  for (let i = 0; i < runeJson[y].slots.length; i += 1) {
-    for (let x = 0; x < runeJson[y].slots[i].runes.length; x += 1) {
-      const imgUrl = `http://ddragon.leagueoflegends.com/cdn/img/${runeJson[y].slots[i].runes[x].icon}`;
-      console.log(imgUrl);
-      // eslint-disable-next-line no-await-in-loop
-      await fetch(imgUrl).then(async (res) => {
-        const body = await res.body;
-        const name = `${runeJson[y].slots[i].runes[x].key}.png`;
-        const downloadWriteStream = fs.createWriteStream(`./src/assets/${name}`);
-        body.pipeTo(stream(downloadWriteStream));
-      });
+async function writeFile(folderName, fileName, data) {
+  try {
+    const folderDir = path.join(rootFolder, 'src/assets/', folderName);
+    const fileExists = await pathExists(folderDir);
+
+    if (!fileExists) {
+      await fsa.mkdir(folderDir, { recursive: true });
     }
+
+    const downloadWriteStream = fs.createWriteStream(path.join(folderDir, fileName));
+
+    data.pipeTo(stream(downloadWriteStream));
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
   }
+}
+
+const getAndSaveImage = async (icon, folder, name) => {
+  const imgUrl = `${DDRAGON_URL}/cdn/img/${icon}`;
+
+  const { body } = await fetch(imgUrl);
+
+  await writeFile(folder, `${name}.png`, body);
+};
+
+try {
+  console.time('getImages');
+  const completeRunes = runePaths.flatMap(({ slots, icon: runePathIcon, key: runePathName }) => {
+    const runePath = [getAndSaveImage(runePathIcon, `runes/${runePathName}`, runePathName)];
+
+    const runesImages = slots.flatMap(({ runes }) =>
+      runes.map(({ icon, key }) => getAndSaveImage(icon, `runes/${runePathName}`, key))
+    );
+
+    return runePath.concat(runesImages);
+  });
+
+  await Promise.all(completeRunes);
+  console.timeEnd('getImages');
+  console.log('Completed data fetching successfully.');
+} catch (err) {
+  console.error('Failed to fetch all images due to: ', err);
 }

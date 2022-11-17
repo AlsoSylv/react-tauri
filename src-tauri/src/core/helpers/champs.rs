3986fbf5b751daf567ca6426e2cb33091463c9c8
Cynthia <josephao424@gmail.com>
2022-11-17 17:52:20 -0500

    Use closure for id

--- src-tauri/src/core/helpers/champs.rs
+++ src-tauri/src/core/helpers/champs.rs
@@ -2,13 +2,11 @@ use crate::{core::data_dragon::structs::DataDragon, errors::DataDragonError};
 
 use super::structs::{ChampionNames, ChampionValue};
 
-fn parse_id(id: &str) -> i64 {
-    id.parse().unwrap()
-}
-
 pub async fn all_champion_names(lang: &str) -> Result<Vec<ChampionNames>, DataDragonError> {
     let mut champions = Vec::new();
     let data_dragon = DataDragon::new(Some(lang)).await;
+    let id  = |id: &str| -> i64 {id.parse().unwrap()};
+    
     match data_dragon {
         Ok(data_dragon) => {
             let champ_json = data_dragon.champion_json().await;
@@ -18,7 +16,7 @@ pub async fn all_champion_names(lang: &str) -> Result<Vec<ChampionNames>, DataDr
                         let key = &champ.id;
                         champions.push(ChampionNames {
                           label: champ.clone().name,
-                          value: ChampionValue { key: champ_key.to_string(), id: parse_id(&champ.key) },
+                          value: ChampionValue { key: champ_key.to_string(), id: id(&champ.key) },
                           url: format!(
                             "https://ddragon.leagueoflegends.com/cdn/{}/img/champion/{}.png",
                             &data_dragon.version,
